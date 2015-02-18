02d79d286fedc7c575acc6adbe9f521e8cf98e5f
Jon Winn <me@jonwinn.com>
2015-02-17 19:36:38 -0800

    cleaned up code; fixed some issues

--- brewutils.sh
+++ brewutils.sh
@@ -5,114 +5,114 @@
 has_brew=$(command -v brew || true)
 
 clean() {
-	echo "Tidying up"
-	brew cleanup;
+  echo "Tidying up"
+  brew cleanup;
 }
 
 doctor() {
-	echo "The doctor is in"
-	brew doctor;
+  echo "The doctor is in"
+  brew doctor;
 }
 
 install() {
-	install=$*
-	echo "Installing $install"
-	brew install $install
+  install=$*
+  echo "Installing $install"
+  brew install $install
 }
 
 remove() {
-	remove=$*
-	echo "Removing $remove"
-	brew remove $remove
-	brew remove $(join <(brew leaves) <(brew deps $remove))
+  remove=$*
+  echo "Removing $remove"
+  brew remove $remove
+  has_deps=$(brew deps $remove)
+  [ $has_deps ] && brew remove $(join <(brew leaves) <(brew deps $remove))
 }
 
 update() {
-	echo "Updating available packages"
-	brew update;
+  echo "Updating available packages"
+  brew update;
 }
 
 upgrade() {
-	echo "Upgrading packages"
-	brew upgrade;
+  echo "Upgrading packages"
+  brew upgrade;
 }
 
 usage() {
-	echo "
-Usage $0: [-cduU] [-i packages] [-r package] args
-	-c:   clean
-	-d:   doctor
-	-h:   show this dialog
-	-u:   update
-	-U:   upgrade
+  echo "
+  Usage $0: [-cduU] [-i packages] [-r package] args
+  -c:   clean
+  -d:   doctor
+  -h:   show this dialog
+  -u:   update
+  -U:   upgrade
 
-	-i:   packages can be a space-delimited set
-	-r:   packages can be a space-delimited set
+  -i:   packages can be a space-delimited set
+  -r:   packages can be a space-delimited set
 
-	args: passed through to $has_brew
-"
+  args: passed through to $has_brew
+  "
 }
 
 if [ $has_brew ]; then
-	clean=
-	doctor=
-	install=
-	remove=
-	update=
-	upgrade=
+  clean=
+  doctor=
+  install=
+  remove=
+  update=
+  upgrade=
 
-	if [ -z "$*" ]; then
-		usage
-		exit
-	fi
+  if [ -z "$*" ]; then
+    usage
+    exit
+  fi
 
-	while getopts cdhuUi:r: opt; do
-		case $opt in
-			c)  clean=1;;
-			d)  doctor=1;;
-			h)  usage && exit;;
-			i)  install="$OPTARG";;
-			r)  remove="$OPTARG";;
-			u)  update=1;;
-			U)  upgrade=1;;
-		  ?)  usage && exit 2;;
-		esac
-	done
+  while getopts cdhuUi:r: opt; do
+    case $opt in
+      c)  clean=1;;
+      d)  doctor=1;;
+      h)  usage && exit;;
+      i)  install="$OPTARG";;
+      r)  remove="$OPTARG";;
+      u)  update=1;;
+      U)  upgrade=1;;
+      ?)  usage && exit 2;;
+    esac
+  done
 
-	shift $(($OPTIND - 1))
-	args=$*
+  shift $(($OPTIND - 1))
+  args=$*
 
-	if [ -n "$update" ]; then
-		update
-	fi
+  if [ -n "$update" ]; then
+    update
+  fi
 
-	if [ -n "$upgrade" ]; then
-		upgrade
-	fi
+  if [ -n "$upgrade" ]; then
+    upgrade
+  fi
 
-	if [ -n "$remove" ]; then
-		remove $remove
-	fi
+  if [ -n "$remove" ]; then
+    remove $remove
+  fi
 
-	if [ -n "$install" ]; then
-		install $install
-	fi
+  if [ -n "$install" ]; then
+    install $install
+  fi
 
-	if [ -n "$clean" ]; then
-		clean
-	fi
+  if [ -n "$clean" ]; then
+    clean
+  fi
 
-	if [ -n "$doctor" ]; then
-		doctor
-	fi
+  if [ -n "$doctor" ]; then
+    doctor
+  fi
 
-	if [ -n "$args" ]; then
-		echo "Passing $args through to $has_brew"
-		brew $args
-	fi
+  if [ -n "$args" ]; then
+    echo "Passing $args through to $has_brew"
+    brew $args
+  fi
 
-	exit 0
+  exit 0
 fi
 
 exit 1
-
--- rsyncbackup.sh
+++ rsyncbackup.sh
@@ -7,144 +7,143 @@ has_rsync=$(command -v rsync || true)
 has_time=$(command -v time || true)
 
 usage() {
-	echo "
-Usage $0: [-bDlv] [-d destination] [-e exclude_file] [-s source] args
-	-b:   backup [$backup_flags]
-	-D:   delete files-not in source or excluded-from estination [$delete_flags]
-	-h:   show this message
-	-l:   use safe links [$link_flags]
-	-t:   dry-run [$test_flags]
-	-v:   verbose [$verb_flags]
+  echo "
+  Usage $0: [-bDlv] [-d destination] [-e exclude_file] [-s source] args
+  -b:   backup [$backup_flags]
+  -D:   delete files-not in source or excluded-from estination [$delete_flags]
+  -h:   show this message
+  -l:   use safe links [$link_flags]
+  -t:   dry-run [$test_flags]
+  -v:   verbose [$verb_flags]
 
-	-d:   the destination folder
-	      [default: RSYNC_DEST or {backup_mount}/{hostname}]
-	-e:   a plain text file newline-delimited for patterns to exclude
-	-s:   the source folder [default: RSYNC_SRC or HOME]
+  -d:   the destination folder
+  [default: RSYNC_DEST or {backup_mount}/{hostname}]
+  -e:   a plain text file newline-delimited for patterns to exclude
+  -s:   the source folder [default: RSYNC_SRC or HOME]
 
-	args: passed through to $has_rsync
-"
+  args: passed through to $has_rsync
+  "
 }
 
 if [ $has_rsync ]; then
   if [ $has_grep ]; then
-		rsync_version=$(rsync --version \
-			| grep -Eo 'version [0-9]+.[0-9]+.[0-9]+' \
-			| grep -Eo '[0-9]+.[0-9]+.[0-9]+')
-		rsync_has_progress=$(echo ${rsync_version%.*} 3.1 \
-			| awk '{ if ($1 >= $2) print 1; else print 0 }')
-	fi
+    rsync_version=$(rsync --version \
+      | grep -Eo 'version [0-9]+.[0-9]+.[0-9]+' \
+      | grep -Eo '[0-9]+.[0-9]+.[0-9]+')
+    rsync_has_progress=$(echo ${rsync_version%.*} 3.1 \
+      | awk '{ if ($1 >= $2) print 1; else print 0 }')
+  fi
 
-	hostname=${HOSTNAME:-$(hostname -s || uname -n)}
-	hostname=${hostname%%.*}
+  hostname=${HOSTNAME:-$(hostname -s || uname -n)}
+  hostname=${hostname%%.*}
 
-	src=${RSYNC_SRC:-$HOME}
-	if [ "$(uname -s)" = "Darwin" ]; then
-		dest=${RSYNC_DEST:="/Volumes/backup"}
-	else
-		dest=${RSYNC_DEST:="/mnt/backup"}
-	fi
-	dest="$dest/$hostname"
+  src=${RSYNC_SRC:-$HOME}
+  if [ "$(uname -s)" = "Darwin" ]; then
+    dest=${RSYNC_DEST:="/Volumes/backup"}
+  else
+    dest=${RSYNC_DEST:="/mnt/backup"}
+  fi
+  dest="$dest/$hostname"
 
-	backup_flags="-rlptDz"
-	delete_flags="--delete --delete-excluded"
-	link_flags="--safe-links"
-	test_flags="--dry-run"
-	verb_flags="-v --progress --human-readable"
+  backup_flags="-rlptDz"
+  delete_flags="--delete --delete-excluded"
+  link_flags="--safe-links"
+  test_flags="--dry-run"
+  verb_flags="-v --progress --human-readable"
 
-	command_args=""
+  command_args=""
 
-	backup=
-	delete=
-	dry_run=
-	exclude=
-	link=
-	verbose=
+  backup=
+  delete=
+  dry_run=
+  exclude=
+  link=
+  verbose=
 
-	if [ -z "$*" ]; then
-		usage
-		exit
-	fi
+  if [ -z "$*" ]; then
+    usage
+    exit
+  fi
 
-	while getopts bd:De:ls:tv opt; do
-		case $opt in
-			b)  backup=1;;
-			d)  dest="$OPTARG";;
-			D)  delete=1;;
-			e)  exclude="$OPTARG";;
-			h)  usage && exit;;
-			l)  link=1;;
-			d)  src="$OPTARG";;
-			t)  dry_run=1;;
-			v)  verbose=1;;
-			?)  usage && exit 2;;
-		esac
-	done
+  while getopts bd:De:ls:tv opt; do
+    case $opt in
+      b)  backup=1;;
+      d)  dest="$OPTARG";;
+      D)  delete=1;;
+      e)  exclude="$OPTARG";;
+      h)  usage && exit;;
+      l)  link=1;;
+      d)  src="$OPTARG";;
+      t)  dry_run=1;;
+      v)  verbose=1;;
+      ?)  usage && exit 2;;
+  esac
+done
 
-	shift $(($OPTIND - 1))
-	args=$*
+shift $(($OPTIND - 1))
+args=$*
 
-	if [ ! -d $src ]; then
-		echo "Source: $src does not exist"
-		exit 2
-	fi
+if [ ! -d $src ]; then
+  echo "Source: $src does not exist"
+  exit 2
+fi
 
-	if [ ! -r $src ]; then
-		echo "Source: $src is not readable"
-		exit 2
-	fi
+if [ ! -r $src ]; then
+  echo "Source: $src is not readable"
+  exit 2
+fi
 
-	if [ ! -d $dest ]; then
-		echo "Destination: $dest does not exist"
-		exit 2
-	fi
+if [ ! -d $dest ]; then
+  echo "Destination: $dest does not exist"
+  exit 2
+fi
 
-	if [ ! -r $dest ]; then
-		echo "Destination: $dest is not writable"
-		exit 2
-	fi
+if [ ! -r $dest ]; then
+  echo "Destination: $dest is not writable"
+  exit 2
+fi
 
-	if [ -n "$backup" ]; then
-		command_args="$command_args $backup_flags"
-	fi
+if [ -n "$backup" ]; then
+  command_args="$command_args $backup_flags"
+fi
 
-	if [ -n "$delete" ]; then
-		command_args="$command_args $delete_flags"
-	fi
+if [ -n "$delete" ]; then
+  command_args="$command_args $delete_flags"
+fi
 
-	if [ -n "$exclude" ] && [ -r $exclude ]; then
-		command_args="$command_args --exclude-from $exclude"
-	fi
+if [ -n "$exclude" ] && [ -r $exclude ]; then
+  command_args="$command_args --exclude-from $exclude"
+fi
 
-	if [ -n "$link" ]; then
-		command_args="$command_args $link_flags"
-	fi
+if [ -n "$link" ]; then
+  command_args="$command_args $link_flags"
+fi
 
-	if [ -n "$dry_run" ]; then
-		command_args="$command_args $test_flags"
-	fi
+if [ -n "$dry_run" ]; then
+  command_args="$command_args $test_flags"
+fi
 
-	if [ -n "$verbose" ]; then
-		command_args="$command_args $verb_flags"
-		if [ "$rsync_has_progress" -eq "1" ]; then
-			command_args="$command_args --info=progress2"
-		fi
-	fi
+if [ -n "$verbose" ]; then
+  command_args="$command_args $verb_flags"
+  if [ "$rsync_has_progress" -eq "1" ]; then
+    command_args="$command_args --info=progress2"
+  fi
+fi
 
-	if [ -n "$args" ]; then
-		echo "Passing $args through to $has_brew"
-		$command_args="$command_args $args"
-	fi
+if [ -n "$args" ]; then
+  echo "Passing $args through to $has_brew"
+  $command_args="$command_args $args"
+fi
 
-	if [ $has_time ]; then
-		echo "Running: time rsync $command_args $src $dest"
-		time rsync $command_args $src $dest
-	else
-		echo "Running: rsync $command_args $src $dest"
-		rsync $command_args $src $dest
-	fi
+if [ $has_time ]; then
+  echo "Running: time rsync $command_args $src $dest"
+  time rsync $command_args $src $dest
+else
+  echo "Running: rsync $command_args $src $dest"
+  rsync $command_args $src $dest
+fi
 
-	exit 0
+exit 0
 fi
 
 exit 1
-
